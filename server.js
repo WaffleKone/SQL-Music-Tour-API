2379730bae9095e967dbf60f390cbd4bc5cc0e60
Jaxon Johnson <jaxoncodes@gmail.com>
2022-08-01 17:27:20 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
-
+const {Sequelize} = require('sequelize')
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +17,15 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+// CONTROLLERS
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
